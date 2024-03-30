d2173a828369040d17a4e38066bed315df8d4175
ParasAnxi <paras.bhandari1010@gmail.com>
2024-03-30 19:11:01 +0530

    14

--- 0014.Longest Common Prefix.py
+++ 0014.Longest Common Prefix.py
@@ -22,3 +22,47 @@ class Solution:
         else:
           right = mid
       return strs[0][:left]
+    
+#Using Trie
+    
+
+class TrieNode:
+  def __init__(self, ch):
+    self.data = ch
+    self.isTerminal = False
+    self.children = [None] * 26
+
+
+class Trie:
+  def __init__(self):
+    self.root = TrieNode('')
+
+  def insert(self, word):
+    curr = self.root
+    for ch in word:
+      index = ord(ch) - ord('a')
+      if not curr.children[index]:
+        curr.children[index] = TrieNode(ch)
+      curr = curr.children[index]
+    curr.isTerminal = True
+
+  def lcp(self):
+    curr = self.root
+    ans = ''
+    while curr:
+      if curr.isTerminal or sum(x is not None for x in curr.children) != 1:
+        break
+      for i, child in enumerate(curr.children):
+        if child:
+          ans += chr(ord('a') + i)
+          curr = child
+          break
+    return ans
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+      trie = Trie()
+      for word in strs:
+        trie. insert(word)
+      return trie.lcp()
